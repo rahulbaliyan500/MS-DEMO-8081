d146f3d89cd3d1e8b39b3ec83f68f1d2f58feb9c
rahulbaliyan404 <rahulbaliyan404@gmail.com>
2022-03-14 22:31:36 +0530

    This is second commit on 14-03-2022.

--- MS-DEMO-8081/src/main/java/com/home/MsDemo8081Application.java
+++ MS-DEMO-8081/src/main/java/com/home/MsDemo8081Application.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsDemo8081Application {
-	
-	// This is first commit 13-03-2022.
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsDemo8081Application.class, args);
 	}
