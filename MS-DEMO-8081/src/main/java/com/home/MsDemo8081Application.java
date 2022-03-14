13f42169bf8567352b63aa4ff8c6e9f9fe2ec0c5
rahulbaliyan404 <rahulbaliyan404@gmail.com>
2022-03-14 19:35:48 +0530

    This is first commit 14-03-2022.

---
+++ MS-DEMO-8081/src/main/java/com/home/MsDemo8081Application.java
@@ -0,0 +1,15 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDemo8081Application {
+	
+	// This is first commit 13-03-2022.
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDemo8081Application.class, args);
+	}
+
+}
---
+++ MS-DEMO-8081/src/test/java/com/home/MsDemo8081ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDemo8081ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
